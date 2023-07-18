1ee515029906ce207e6b54abac3d011a49a64c48
poljudy <franklinjin229@gmail.com>
2023-07-18 09:56:12 -0700

    controller fix

--- controllers/resourcesController.js
+++ controllers/resourcesController.js
@@ -1,49 +1,47 @@
 const db = require("../models");
 
-// Defining methods for the db Controller
 module.exports = {
-getbycatagory: function(req, res) {
-db.Resources
-.findOne({ catagory: req.params.catagory}, req.body)
-.then(dbresult => res.json(dbresult))
-.catch(err => res.status(404).json(err));
-},
-editbycatagory: function(req, res) {
-    db.Resources
-    .findOneAndUpdate({ catagory: req.params.catagory }, req.body)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
-},
-createbycatagory: function(req, res) {
-    db.Resources
-    .create({ catagory: req.params.catagory }, req.body)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
-},
-deletebyid: function(req, res) {
-  db.Resources
-  .findById({ _id: req.params.id })
-    .then(dbModel => dbModel.remove())
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
-},
-getAll: function(req, res) {
+  getbycatagory: function (req, res) {
+    db.Resources.findOne({ catagory: req.params.catagory }, req.body)
+      .then((dbresult) => res.json(dbresult))
+      .catch((err) => res.status(404).json(err));
+  },
+
+  editbycatagory: function (req, res) {
+    db.Resources.findOneAndUpdate({ catagory: req.params.catagory }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+
+  createbycatagory: function (req, res) {
+    db.Resources.create({ catagory: req.params.catagory }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+
+  deletebyid: function (req, res) {
+    db.Resources.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+
+  getAll: function (req, res) {
     db.Resources.find(req.query)
-    .sort({rating: -1})
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .sort({ rating: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  create: function(req, res) {
-    console.log("req.body")
-    console.log(req.body)
+  create: function (req, res) {
+    console.log("req.body");
+    console.log(req.body);
     db.Resources.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: function (req, res) {
     db.Resources.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-// }
 };
--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,37 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the db Controller
 module.exports = {
-deletebyid: function(req, res) {
-  db.Staff
-  .findById({ _id: req.params.id })
-    .then(dbModel => dbModel.remove())
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
-},
-getAll: function(req, res) {
-    db.Staff.find(req.query)
-    .sort({rating: -1})
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  deletebyid: function (req, res) {
+    db.Staff.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  bynameall: function(req, res) {
+  getAll: function (req, res) {
     db.Staff.find(req.query)
-    .sort({name: 1})
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .sort({ rating: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  create: function(req, res) {
-    console.log("req.body")
-    console.log(req.body)
+  bynameall: function (req, res) {
+    db.Staff.find(req.query)
+      .sort({ name: 1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    console.log("req.body");
+    console.log(req.body);
     db.Staff.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: function (req, res) {
     db.Staff.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-// }
 };
