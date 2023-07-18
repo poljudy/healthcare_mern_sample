e3c7e59a0de77a853438490418204ffac0896b1a
poljudy <franklinjin229@gmail.com>
2023-07-18 09:54:45 -0700

    fix middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,14 @@
 const jwt = require('jsonwebtoken');
-// const config = require('config');
-// const config = process.env.jwtSecret
 
 module.exports = function(req, res, next) {
   const jwtconfig = process.env.jwtSecret
-  // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if not token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
-    // const decoded = jwt.verify(token, config.get('jwtSecret'));
     const decoded = jwt.verify(token, jwtconfig);
 
     req.user = decoded.user;
