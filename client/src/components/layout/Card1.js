2898e731f621410f4494103668fb485a0b11d5d0
poljudy <franklinjin229@gmail.com>
2023-07-26 09:39:29 -0700

    update context
    Co-authored-by: derpsol <jakecalison918@gmail.com>

--- client/src/components/layout/Card1.js
+++ client/src/components/layout/Card1.js
@@ -1,97 +1,119 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
 // import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import CardContent from "@material-ui/core/CardContent";
 // import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import theme from '../theme/Theme';
-import { Link } from '@material-ui/core';
+import Typography from "@material-ui/core/Typography";
+import theme from "../theme/Theme";
+import { Link } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    height: '100%',
-    textAlign: 'center',
+    height: "100%",
+    textAlign: "center",
     // backgroundColor: theme.palette.secondary.main,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     border: 1,
-    borderWidth: 'thick',
-    borderColor : theme.palette.secondary.main
+    borderWidth: "thick",
+    borderColor: theme.palette.secondary.main,
     // maxWidth: 100
   },
   bullet: {
     // display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
     // fontSize: 14,
     color: theme.palette.secondary.dark,
-    [theme.breakpoints.down('md')]: {
+    [theme.breakpoints.down("md")]: {
       fontSize: 28,
       // textAlign: 'justify'
-  }
+    },
   },
   pos: {
     marginBottom: 12,
   },
   theming: {
     color: theme.palette.primary.main,
-    [theme.breakpoints.down('md')]: {
+    [theme.breakpoints.down("md")]: {
       fontSize: 18,
       // textAlign: 'justify'
-  }
+    },
   },
-footer: {
-  fontSize: 25,
-  [theme.breakpoints.down('md')]: {
-    fontSize: 15,
-    // textAlign: 'justify'
-}
-}
+  footer: {
+    fontSize: 25,
+    [theme.breakpoints.down("md")]: {
+      fontSize: 15,
+      // textAlign: 'justify'
+    },
+  },
 });
 
-const Card1 = props => {
+const Card1 = (props) => {
   const classes = useStyles();
   // const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>
-      <CardContent >
+      <CardContent>
         {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
     {props.title}
         </Typography> */}
         <br />
-        <Typography variant="subtitle1" component="h2" className={classes.title}>
-        {props.title}
+        <Typography
+          variant="subtitle1"
+          component="h2"
+          className={classes.title}
+        >
+          {props.title}
         </Typography>
         {!props.link3 ? null : (
-<Link href={props.link3} target="_blank" rel="noopener">
-        <Typography variant="subtitle2" component="p" className={classes.theming}>
-          {props.main}
-          <br />
-          {props.main2}
-          </Typography>
+          <Link href={props.link3} target="_blank" rel="noopener">
+            <Typography
+              variant="subtitle2"
+              component="p"
+              className={classes.theming}
+            >
+              {props.main}
+              <br />
+              {props.main2}
+            </Typography>
           </Link>
-          )}
-          {props.link3 ? null : (
-            <Typography variant="subtitle2" component="p" className={classes.theming}>
-          {props.main}
-          <br />
-          {props.main2}
+        )}
+        {props.link3 ? null : (
+          <Typography
+            variant="subtitle2"
+            component="p"
+            className={classes.theming}
+          >
+            {props.main}
+            <br />
+            {props.main2}
           </Typography>
-          )}
-          <br />
-          <Link href={"tel:" + props.link1} target="_blank" rel="noopener">
-          <Typography className={classes.footer} variant="body2" color="textSecondary" gutterBottom>
-          {props.link1}
-        </Typography>
+        )}
+        <br />
+        <Link href={"tel:" + props.link1} target="_blank" rel="noopener">
+          <Typography
+            className={classes.footer}
+            variant="body2"
+            color="textSecondary"
+            gutterBottom
+          >
+            {props.link1}
+          </Typography>
         </Link>
-        <Link href={"mailto:" + + props.link2} target="_blank" rel="noopener">
-        <Typography className={classes.footer} variant="body2" color="textSecondary" gutterBottom>
-          {props.link2}
-        </Typography>
+        <Link href={"mailto:" + +props.link2} target="_blank" rel="noopener">
+          <Typography
+            className={classes.footer}
+            variant="body2"
+            color="textSecondary"
+            gutterBottom
+          >
+            {props.link2}
+          </Typography>
         </Link>
         {/* <Typography className={classes.pos} color="textSecondary">
           {props.link2}
@@ -103,5 +125,5 @@ const Card1 = props => {
       </CardActions> */}
     </Card>
   );
-}
+};
 export default Card1;
